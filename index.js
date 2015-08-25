98752d061ed022023be6b123d528edf0db279e24
Maarten Cautreels <maarten.cautreels@gmail.com>
2015-08-25 20:59:52 +0200

    Replaced FB integration by random quotes

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 var TelegramBot = require('node-telegram-bot-api');
-var Facebook = require('facebook-node-sdk');
+var janMessages = require('./jan-messages');
 
 console.log("Starting up!");
-
-var facebook = new Facebook({ appId: '1479298715703680', secret: 'c16f2f97780cab8af4c13b7458b0aecf' });
-
-
-
-//User-ID Jan: 1110914570
-//facebook.api('/1110914570/feed', function(err, data) {
-//    console.log(err);
-//    console.log(data);
-//});
 
 
 var token = '135201031:AAFIlf0xObcFn9CiPCtkWbUTT7SITFQ9Mss';
 
 var bot = new TelegramBot(token, {polling: true});
-bot.on('photo', function (msg) {
+bot.on('text', function (msg) {
   var chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, 'Wie goed doet; Goed ontmoet!');
+  if(msg.text === '/quote') {
+  	var message = janMessages[getRandomNumber(janMessages.length) - 1];
+
+    bot.sendMessage(chatId, message);
+  }
 });
+
+var getRandomNumber = function (max) {
+	var randomNumber = Math.floor((Math.random() * max) + 1);
+	return randomNumber;
+}
---
+++ jan-messages.js
@@ -0,0 +1,13 @@
+var janMessages = [
+	'Wie goed doet, goed ontmoet.',
+	'‪#‎watvoelikmeeengelukkigmensalsikanderenkanblijmaken‬',
+	'Dress to impress',
+	'Keep young and fit',
+	'#‎geholpenmensenzijnmensendiezichzelfhelpeninhetleven‬',
+	'‪#‎Echtemannenzorgenvoorhuneigensuccesinhetleven',
+	'#‎keepyoungfitandhappypeople‬',
+	'De kunst des levens is tevreden (leren) zijn met het "tevredene',
+	'#vlaamsevrouwenvallenoplosersdiegeenrottebalhebbenenbovendienooknogeenseenslechtkarakternietsnutarmoeizaaierzijnendannogeensnulkommanulklassehebben'
+];
+
+module.exports = janMessages;
