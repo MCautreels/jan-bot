1fec38cca5116ba0945730058e12d1af9db4830f
Glenn Heylen <heyleng@gmail.com>
2015-08-25 16:01:23 +0200

    no message

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var TelegramBot = require('node-telegram-bot-api');
 var Facebook = require('facebook-node-sdk');
- 
- console.log("Hello World");
- 
+
+console.log("Starting up!");
+
+var facebook = new Facebook({ appId: '1479298715703680', secret: 'c16f2f97780cab8af4c13b7458b0aecf' });
+
+
+
+//User-ID Jan: 1110914570
+//facebook.api('/1110914570/feed', function(err, data) {
+//    console.log(err);
+//    console.log(data);
+//});
+
+
 var token = '135201031:AAFIlf0xObcFn9CiPCtkWbUTT7SITFQ9Mss';
-// Setup polling way 
+
 var bot = new TelegramBot(token, {polling: true});
-bot.on('text', function (msg) {
+bot.on('photo', function (msg) {
   var chatId = msg.chat.id;
-  // photo can be: a file path, a stream or a Telegram file_id 
-//  var photo = 'cats.png';
-  bot.sendMessage(chatId, 'HALLO TEST');
+
+  bot.sendMessage(chatId, 'Wie goed doet; Goed ontmoet!');
 });
