f5d5faead45fb025d26485eb3f85effe2f01a3ae
Glenn Heylen <heyleng@gmail.com>
2015-08-25 12:34:04 +0200

    no message

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+var TelegramBot = require('node-telegram-bot-api');
+var Facebook = require('facebook-node-sdk');
+ 
+ console.log("Hello World");
+ 
+//var token = 'YOUR_TELEGRAM_BOT_TOKEN';
+// Setup polling way 
+//var bot = new TelegramBot(token, {polling: true});
+//bot.on('text', function (msg) {
+//  var chatId = msg.chat.id;
+//  // photo can be: a file path, a stream or a Telegram file_id 
+//  var photo = 'cats.png';
+//  bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+//});
