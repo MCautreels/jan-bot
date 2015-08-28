4908a03e1d458334aa7ac8e2b17e16bc8bb303f3
Siebe Sysmans <siebe@Siebes-MacBook-Pro.local>
2015-08-28 12:23:45 +0200

    Goodmorning - name replacements

--- goodmorning/index.js
+++ goodmorning/index.js
@@ -1,10 +1,17 @@
 module.exports = function (telegramBot)  {
 	telegramBot.on('text', function (msg) {
 	  var chatId = msg.chat.id;
-	  var user = msg.from.first_name;
+	  var userFirstName = msg.from.first_name;
+	  var userLastName = msg.from.last_name;
+
+	  user = userFirstName;
+
+	  if(userFirstName === 'Wouter' && userLastName === 'Thys') user = 'Flappy';
+	  if(userFirstName === 'Wout') user = 'Van Immerseelke';
+	  if(userFirstName === 'Siebe') user = 'oh almachtige god';
 
 	  if((msg.text.toLowerCase().indexOf('goeiemorgen') > -1 || msg.text.toLowerCase().indexOf('goedemorgen') > -1) && msg.text.toLowerCase().indexOf('jan')) {
-	    telegramBot.sendMessage(chatId, 'Goeiemorgen ' + user);
+	    telegramBot.sendMessage(chatId, 'Goeiemorgen ' + user + '!');
 	  }
 	});
 }
