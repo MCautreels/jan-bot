22a5694b566efcd3e5cc3a16152595e190e9bc34
Glenn Heylen <heyleng@gmail.com>
2015-08-28 18:08:40 +0200

    cleverbot foefelaria

--- cleverbot/index.js
+++ cleverbot/index.js
@@ -1,19 +1,26 @@
 //var Cleverbot = require('cleverbot.io');
 //
-//module.exports = function (telegramBot) {
-//    console.log("Starting cleverbot..");
+//module.exports = function (/*telegramBot*/) {
+//    console.log("Adding cleverbot feature..");
 //
-//    cleverbot = new Cleverbot("YOUR_API_USER", "YOUR_API_KEY");
-//    cleverbot.setNick("JANBOT_435h3426h4v5gj623jh4g523");
+//    cleverbot = new Cleverbot("Hkh3j4X590P2mzCT", "8VtfotmZrhCJOPpm17h0Yaen1uhd2EgM", "JANBOT_7wa4cxvjvb4l68y4ta");
 //
 //    function callback(err, response) {
-//        console.error(err);
-//        console.log(response);
+//        //console.error(err);
+//        //console.log(response);
+//        telegramBot.sendMessage(response.chatId, message);
 //    }
 //
 //    cleverbot.create(callback);
+//    cleverbot.ask('Hi there', callback);
 //
+//
+//
+//    //var msg.test = "Jan, hi there!";
 //    telegramBot.on('text', function (msg) {
-//        cleverbot.ask(msg, callback);
+//    if (msg.text.toLowerCase().indexOf("jan, ") > -1) {
+//        msg.chat.id
+//        cleverbot.ask(msg.text, callback);
+//    }
 //    });
 //}
