91aa9cd7a0edfbfb67d3d4209924062a4cdb3d21
Bart <bart@veno.be>
2015-08-28 20:44:08 +0200

    hashtagger

--- random-quotes/index.js
+++ random-quotes/index.js
@@ -1,4 +1,5 @@
 var janMessages = require('./jan-messages');
+var _ = require('lodash');
 
 module.exports = function (telegramBot)  {
 	telegramBot.on('text', function (msg) {
@@ -9,7 +10,18 @@ module.exports = function (telegramBot)  {
 
 	    telegramBot.sendMessage(chatId, message);
 	  }
+	  else if(msg.text.indexOf('#') > -1) {
+		telegramBot.sendMessage(chatId, geefRandomHash());
+	  }
+	  
 	});
+
+	var geefRandomHash = function(){
+		var hastags = _.where(janMessages, function(quote){ return quote.indexOf('#') === 0;});
+		var message = hastags[getRandomNumber(hastags.length) - 1];
+		
+		return message;
+	}
 
 	var getRandomNumber = function (max) {
 		var randomNumber = Math.floor((Math.random() * max) + 1);
