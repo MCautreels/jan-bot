dd82edbcfd5c40b17cba9c7468621da95f1cf1ba
Glenn Heylen <heyleng@gmail.com>
2015-09-01 00:54:11 +0200

    jandeweerman!

---
+++ jan-de-weerman/index.js
@@ -0,0 +1,21 @@
+weather = require('openweathermap');
+var config = require('./config');
+
+module.exports = function (telegramBot) {
+    console.log("Toggling JAN-DE-WEERMAN feature: ACTIVATED");
+
+    weather.defaults(config);
+
+    telegramBot.on('text', function (msg) {
+        if (msg.text.toLowerCase().indexOf('/weer') > -1) {
+            var messageText = msg.text.replace('/weer', '').trim();
+
+            weather.now({q: messageText}, function (err, json) {
+                console.error(err);
+                if (err == null) {
+                    telegramBot.sendMessage(msg.chat.id, "Vrienden! daar is het " + Math.floor(json['main']['temp']) + " graden. #alsdezonschijntishetmooiweer");
+                }
+            });
+        }
+    });
+}
