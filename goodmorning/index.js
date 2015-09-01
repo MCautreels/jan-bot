6d6b91002db3437c6c519049e83c93ccbe5ae6b7
Maarten Cautreels <maarten.cautreels@persgroep.be>
2015-09-01 09:36:42 +0200

    Revert "Hehe"
    This reverts commit 44a49d739d2aef31e6c0fa602508b0c9ca9a6fe0.

--- goodmorning/index.js
+++ goodmorning/index.js
@@ -28,11 +28,16 @@ module.exports = function (telegramBot)  {
 	  
 	  var deWaarheidRegEx = /.*waar.*(he|eh).*jan.*/;
 	  if(deWaarheidRegEx.test(message)) {
-		if(Math.random()<.5){
+		if(getRandomNumber(1) === 0){
 			telegramBot.sendMessage(chatId, 'Dat is waar, vriend!');
 		} else {
 			telegramBot.sendMessage(chatId, 'Denk wat je wil, vriend. Ik heb andere zaken te doen, en jij vast ook.');
 		}
 	  }
 	});
+	
+	var getRandomNumber = function (max) {
+		var random = new Random(Random.engines.mt19937().autoSeed());
+		return random.integer(0, max);
+	}
 }
